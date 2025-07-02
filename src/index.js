8509c497233c2a5853c8a67fde31677c479b804f
Mahim Ibrahimi <mahimibrahimi@gmail.com>
2025-07-02 15:18:32 +0330

    Implement Nav functionality
    - Add loader function to utils.js
    - loader function will change the content of div#content with the given page content
    - Implement the nav button functionality (load the page when clicked)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import "./styles.css";
+import {loader} from "./utils";
 
 import createHome from "./home";
 import createMenu from "./menu";
 import createAbout from "./about";
 
-// createHome();
-// createMenu();
-createAbout();
+document.querySelector("#home-btn").addEventListener("click", () => {
+    loader(createHome)
+});
+
+document.querySelector("#menu-btn").addEventListener("click", () => {
+    loader(createMenu)
+});
+
+document.querySelector("#about-btn").addEventListener("click", () => {
+    loader(createAbout)
+});
+
+loader(createHome);
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,13 @@
-export function createTag(tag, text) {
+function createTag(tag, text) {
     const element = document.createElement(tag);
     element.textContent = text;
     return element;
 }
+
+function loader(loadPage) {
+    const content = document.querySelector("#content");
+    content.textContent = "";
+    content.appendChild(loadPage());
+}
+
+export { createTag, loader };
